d6d99561859410c9385be4fa03edc4e23d15b00e
tommy <thomas_lasersohn@brown.edu>
2024-02-24 15:10:37 -0500

    moved gearup mock into project

---
+++ mock/src/components/REPLHistory.tsx
@@ -0,0 +1,17 @@
+import "../styles/main.css";
+
+interface REPLHistoryProps {
+  // TODO: Fill with some shared state tracking all the pushed commands
+  history: String[];
+}
+export function REPLHistory(props: REPLHistoryProps) {
+  return (
+    <div className="repl-history">
+      {/* This is where command history will go */}
+      {/* TODO: To go through all the pushed commands... try the .map() function! */}
+      {props.history.map((command) => (
+        <p>{command}</p>
+      ))}
+    </div>
+  );
+}
